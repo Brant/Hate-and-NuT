67c7aa963da4ee66ed9f5258fd5dd6e9069fcb07
Brant Steen <brant.steen@gmail.com>
2013-07-05 09:45:51 -0400

    Removing donation stuff so I can push these other things live

--- hancom/sponsorship/forms.py
+++ hancom/sponsorship/forms.py
@@ -8,8 +8,6 @@ from hancom.sponsorship.models import SponsorshipInquiry
 from noodles.forms import EmailInput
 
 
-
-
 class InquiryForm(forms.ModelForm):
     name = forms.CharField(label="Your name", help_text="So I know what to call you")
     email = forms.CharField(label="Your email", help_text="So I can contact you", widget=EmailInput())
