4e8e04bcde13e84e46c0a18218b4a7594c7b73ee
Brant Steen <brant.steen@gmail.com>
2013-07-05 10:55:21 -0400

    Sometimes javascript is stupid...

--- assets/themes/hancom/static/js/gaEvents.js
+++ assets/themes/hancom/static/js/gaEvents.js
@@ -2,74 +2,55 @@
  * Google Analytics Events
  */
 $(function(){
+	
+	var pushAnalyticsEvent = function(cat, ev, label, val){
+		if (typeof _gaq === 'undefined') {
+			if(val){
+				ga('send', 'event', cat, ev, label, val);	
+			}
+			else{
+				ga('send', 'event', cat, ev, label);
+			}
+		}
+		else{
+			if (val){
+				_gaq.push(['_trackEvent', cat, ev, label, val]);
+			}
+			else{
+				_gaq.push(['_trackEvent', cat, ev, label]);	
+			}
+		}
+	};
+	
 	var thisPath = (window.location.pathname + window.location.search);
 	
 	$(".sponsorship-ad-wrapper").delegate(".sponsorship-ad", "click", function(ev){
 		var clicked = $(this);
-		if (_gaq){
-			_gaq.push(['_trackEvent', 'Ad Click', clicked.data("data-sponsorship-ad-code"), thisPath]);	
-		}
-		else{
-			ga('send', 'event', 'Ad Click', clicked.data("data-sponsorship-ad-code"), thisPath);
-		}
-		
+		pushAnalyticsEvent("Ad Click", clicked.data("data-sponsorship-ad-code"), thisPath);
 	});
 	
 	$(".comic-nav-list-item-left").click(function(){
-		if (_gaq){
-			_gaq.push(['_trackEvent', 'Navigate', "Last Comic", thisPath]);	
-		}
-		else{
-			ga('send', 'event', 'Navigate', "Last Comic", thisPath);
-		}
+		pushAnalyticsEvent("Navigate", "Last Comic", thisPath);
 	});
 	
 	$(".comic-nav-list-item-right").click(function(){
-		if (_gaq){
-			_gaq.push(['_trackEvent', 'Navigate', "Next Comic", thisPath]);	
-		}
-		else{
-			ga('send', 'event', 'Navigate', "Next Comic", thisPath);
-		}
-		
+		pushAnalyticsEvent("Navigate", "Next Comic", thisPath);
 	});
 	
 	$(".pg-foot-wrap a").click(function(ev){
 		ev.preventDefault();
-		if (_gaq){
-			_gaq.push(['_trackEvent', 'Navigate', "Footer", $(ev.target).text()]);	
-		}
-		else{
-			ga('send', 'event', 'Navigate', "Footer", $(ev.target).text());
-		}
+		pushAnalyticsEvent("Navigate", "Footer", $(ev.target).text());
 	});
 	
 	$(".pg-head .donate-button").click(function(ev){
-		if (_gaq){
-			_gaq.push(['_trackEvent', 'Navigate', "Header", "Donate"]);	
-		}
-		else{
-			ga('send', 'event', 'Navigate', "Header", "Donate");
-		}
-		
+		pushAnalyticsEvent("Navigate", "Header", "Donate");
 	});
 	
 	$(".s .donate-button").click(function(ev){
-		if (_gaq){
-			_gaq.push(['_trackEvent', 'Navigate', "Sidebar", "Donate"]);	
-		}
-		else{
-			ga('send', 'event', 'Navigate', "Sidebar", "Donate");
-		}
-		
+		pushAnalyticsEvent("Navigate", "Sidebar", "Donate");
 	});
 	
 	$(".s .social .social-icon").click(function(ev){
-		if (_gaq){
-			_gaq.push(['_trackEvent', 'Navigate', "Sidebar", $(ev.target).text()]);	
-		}
-		else{
-			ga('send', 'event', 'Navigate', "Sidebar", $(ev.target).text());
-		}
+		pushAnalyticsEvent("Navigate", "Sidebar", $(ev.target).text());
 	});
 });
