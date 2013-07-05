47b92fd2c268a1da9af01f67d69e5f91097bc707
Brant Steen <brant.steen@gmail.com>
2013-07-05 10:41:56 -0400

    Events for 'Universal Analytics'

--- assets/themes/hancom/static/js/gaEvents.js
+++ assets/themes/hancom/static/js/gaEvents.js
@@ -6,31 +6,70 @@ $(function(){
 	
 	$(".sponsorship-ad-wrapper").delegate(".sponsorship-ad", "click", function(ev){
 		var clicked = $(this);
-		_gaq.push(['_trackEvent', 'Ad Click', clicked.data("data-sponsorship-ad-code"), thisPath]);
+		if (_gaq){
+			_gaq.push(['_trackEvent', 'Ad Click', clicked.data("data-sponsorship-ad-code"), thisPath]);	
+		}
+		else{
+			ga('send', 'event', 'Ad Click', clicked.data("data-sponsorship-ad-code"), thisPath);
+		}
+		
 	});
 	
 	$(".comic-nav-list-item-left").click(function(){
-		_gaq.push(['_trackEvent', 'Navigate', "Last Comic", thisPath]);
+		if (_gaq){
+			_gaq.push(['_trackEvent', 'Navigate', "Last Comic", thisPath]);	
+		}
+		else{
+			ga('send', 'event', 'Navigate', "Last Comic", thisPath);
+		}
 	});
 	
 	$(".comic-nav-list-item-right").click(function(){
-		_gaq.push(['_trackEvent', 'Navigate', "Next Comic", thisPath]);
+		if (_gaq){
+			_gaq.push(['_trackEvent', 'Navigate', "Next Comic", thisPath]);	
+		}
+		else{
+			ga('send', 'event', 'Navigate', "Next Comic", thisPath);
+		}
+		
 	});
 	
 	$(".pg-foot-wrap a").click(function(ev){
 		ev.preventDefault();
-		_gaq.push(['_trackEvent', 'Navigate', "Footer", $(ev.target).text()]);
+		if (_gaq){
+			_gaq.push(['_trackEvent', 'Navigate', "Footer", $(ev.target).text()]);	
+		}
+		else{
+			ga('send', 'event', 'Navigate', "Footer", $(ev.target).text());
+		}
 	});
 	
 	$(".pg-head .donate-button").click(function(ev){
-		_gaq.push(['_trackEvent', 'Navigate', "Header", "Donate"]);
+		if (_gaq){
+			_gaq.push(['_trackEvent', 'Navigate', "Header", "Donate"]);	
+		}
+		else{
+			ga('send', 'event', 'Navigate', "Header", "Donate");
+		}
+		
 	});
 	
 	$(".s .donate-button").click(function(ev){
-		_gaq.push(['_trackEvent', 'Navigate', "Sidebar", "Donate"]);
+		if (_gaq){
+			_gaq.push(['_trackEvent', 'Navigate', "Sidebar", "Donate"]);	
+		}
+		else{
+			ga('send', 'event', 'Navigate', "Sidebar", "Donate");
+		}
+		
 	});
 	
 	$(".s .social .social-icon").click(function(ev){
-		_gaq.push(['_trackEvent', 'Navigate', "Sidebar", $(ev.target).text()]);
+		if (_gaq){
+			_gaq.push(['_trackEvent', 'Navigate', "Sidebar", $(ev.target).text()]);	
+		}
+		else{
+			ga('send', 'event', 'Navigate', "Sidebar", $(ev.target).text());
+		}
 	});
 });
