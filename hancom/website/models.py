d5446cec89d86b358d4357a3da30a349a83ceba2
Brant Steen <brant.steen@gmail.com>
2013-08-11 12:47:22 -0400

    Just stashing this away in a branch

---
+++ hancom/website/models.py
@@ -0,0 +1,12 @@
+"""
+Hate and NuT Website Models
+"""
+from django.db import models
+
+from noodles.ecommerce.models import PayPalMetaTemplate, PayPalPurchasable
+
+
+class HancomPayPalMeta(PayPalMetaTemplate):
+    """
+    """
+    pass
