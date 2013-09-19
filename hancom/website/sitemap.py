c189261b36020383f9c9a0993f7e624d4365c12c
Brant <brant.steen@gmail.com>
2013-09-19 12:52:15 -0400

    minor sitemap update

--- hancom/website/sitemap.py
+++ hancom/website/sitemap.py
@@ -10,7 +10,7 @@ from hancom.website.queries import published_comics
 
 class Page(object):
     """
-    Simple object to hold attributes for 
+    Simple object to hold attributes for
         webpage sitemap entries
     """
     def __init__(self, name, location, lastmod, priority, changefreq):
@@ -28,23 +28,23 @@ class WebsiteSitemap(Sitemap):
     def items(self):
         return [
             Page("Homepage", reverse("home"), published_comics()[0].date, "1.0", "daily"),
-            Page("Archive", reverse("archive_index"), published_comics()[0].date, ".7", "weekly"),
+            Page("Archive", reverse("archive_index"), published_comics()[0].date, ".7", "daily"),
 #             Page("Donate", reverse("donate"), date(2013, 7, 1), ".5", "monthly"),
             Page("Characters", reverse("characters_index"), date(2013, 7, 5), ".5", "monthly"),
             Page("Advertise", reverse("become_an_advertiser"), date(2013, 7, 1), ".5", "monthly"),
             Page("Sponsor", reverse("become_a_sponsor"), date(2013, 7, 1), ".5", "monthly"),
             Page("Contact", reverse("contact_me"), date(2013, 8, 1), ".5", "monthly"),
         ]
-    
+
     def location(self, item):
         return item.location
-    
+
     def lastmod(self, item):
         return item.lastmod
-    
+
     def priority(self, item):
         return item.priority
-    
+
     def changefreq(self, item):
         return item.changefreq
-        
+
