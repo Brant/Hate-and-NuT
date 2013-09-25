dc86de0e23f898085adebf9795842d0ec1b8372a
Brant <brant.steen@gmail.com>
2013-09-25 12:03:19 -0400

    fiddling with middleware order

--- hancom/settings.py
+++ hancom/settings.py
@@ -119,12 +119,12 @@ MIDDLEWARE_CLASSES = (
 if not DEBUG:
     MIDDLEWARE_CLASSES = (
         'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.cache.UpdateCacheMiddleware'
-        'django.middleware.common.CommonMiddleware',
         # 'django.middleware.gzip.GZipMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.cache.UpdateCacheMiddleware'
+        'django.middleware.common.CommonMiddleware',
         'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
         # Uncomment the next line for simple clickjacking protection:
         # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
