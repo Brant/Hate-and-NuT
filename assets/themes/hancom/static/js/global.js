3b323789cec87df6582418be6c736ecfc1f8db5e
Brant Steen <brant.steen@gmail.com>
2013-10-18 13:33:13 -0400

    assigning 'r' key to random link

--- assets/themes/hancom/static/js/global.js
+++ assets/themes/hancom/static/js/global.js
@@ -4,12 +4,33 @@ $(function(){
 	var prevComic = $("#prevComic");
 	var nextComic = $("#nextComic");
 
+	var randomLinks = $(".comic-nav-list-item-random a");
+	if (randomLinks.length > 0){
+
+		var chronology = randomLinks.first().data("currentchronology");
+
+		$.get("/comic/random/?current_comic=" + chronology).done(function(data){
+			randomLinks.attr("href", data.url);
+
+			var randomComic = $("#randomComic");
+			var randomComicHref = randomComic.attr("href");
+
+			$(document).bind('keydown', 'r', function(){
+				window.location = randomComicHref;
+			});
+
+		});
+	}
+
+
 	if (nextComic.length > 0){
 		nextComicHref = nextComic.attr("href");
+		$("img.comic").wrap("<a href='" + nextComicHref + "' />");
 	}
 	if (prevComic.length > 0){
 		prevComicHref = prevComic.attr("href");
 	}
+
 
 	if (prevComicHref){
 		$(document).bind('keydown', 'a', function(){
@@ -28,21 +49,6 @@ $(function(){
 		$(document).bind('keydown', 'k', function(){
 			window.location = nextComicHref;
 		});
-	}
-
-	var randomLinks = $(".comic-nav-list-item-random a");
-	if (randomLinks.length > 0){
-		var chronology = randomLinks.first().data("currentchronology");
-		$.get("/comic/random/?current_comic=" + chronology).done(function(data){
-			randomLinks.attr("href", data.url);
-		});
-	}
-
-
-	var nextLink = $("#nextComic");
-	if (nextLink.length > 0){
-		var nextHref = nextLink.attr("href");
-		$("img.comic").wrap("<a href='" + nextHref + "' />");
 	}
 
 });
